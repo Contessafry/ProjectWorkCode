06f687fa0ec7d08434110a3bb8342799ed90a09d
Francesco Marino <francescofriy@gmail.com>
2024-03-22 14:00:17 +0100

    feat/Main added pages route

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import PageProduct from "./pages/PageProduct";
+import PageDashboard from "./pages/PageDashBoard";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/products/:productId" element={<PageProduct />} />
+        <Route path="/dashboard" element={<PageDashboard />} />
+        <Route
+          path="*"
+          element={
+            <div>
+              Page not found <button>RETURN</button>
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
