a5867fa482d7ec024d5fecc95a2f060d0fb3e06a
giorgio sancarlo <sancarlogiorgio@gmail.com>
2024-03-22 10:59:52 +0100

    feat/Card

---
+++ src/components/Cards.tsx
@@ -0,0 +1,26 @@
+import {} from "react";
+
+interface Props {
+  product: Any;
+}
+
+export default function Card({ product }: Props) {
+  const { title, price, category, description, image } = product;
+  return (
+    <div>
+      <img src={image} alt="" />
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+      <h3>{price}</h3>
+      <h4>{category}</h4>
+
+      <Card
+        color="neutral"
+        invertedColors={false}
+        orientation="vertical"
+        size="md"
+        variant="soft"
+      />
+    </div>
+  );
+}
