72617f30fba867bd04f0c8c01054e223dd4b3d85
Alessandro Governale <samuele_governale@live.it>
2024-03-22 14:36:36 +0100

    added navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,47 @@
-function navbar(){
+
+import Box from '@mui/joy/Box';
+import List from '@mui/joy/List';
+import ListDivider from '@mui/joy/ListDivider';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+
+
+export default function navbar(){
 
 
     return(
-        <nav>
-            <button type="button">login</button>
-            <button type="button">cart</button>
-        </nav>
+       
+        <Box component="nav" aria-label="My site" sx={{ flexGrow: 1 }}>
+                <List role="menubar" orientation="horizontal">
+                    <ListItem role="none">
+                        <ListItemButton
+                            role="menuitem"
+                            component="a"
+                            href="#horizontal-list"
+                            aria-label="Home"
+                        >
+                            
+                    </ListItemButton>
+                    </ListItem>
+                    <ListDivider />
+                    <ListItem role="none">
+                        <ListItemButton role="menuitem" component="a" href="#horizontal-list">
+                            Products
+                        </ListItemButton>
+                    </ListItem>
+                    <ListDivider />
+                    <ListItem role="none">
+                        <ListItemButton role="menuitem" component="a" href="#horizontal-list">
+                            Blog
+                        </ListItemButton>
+                    </ListItem>
+                   
+                    
+                </List>
+            </Box>
+
+
+
+
     )
 }
